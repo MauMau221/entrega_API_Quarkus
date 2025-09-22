06dd163a2a2a94a11f777d7b0543ac5239c3d479
MauMau221 <mauriciobgsp@gmail.com>
2025-09-22 11:41:27 -0300

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+-- Inserindo produtos de teste
+INSERT INTO Produto(id, nome, descricao, preco) VALUES (nextval('hibernate_sequence'), 'Notebook', 'Notebook Dell 16GB RAM', 4500.0);
+INSERT INTO Produto(id, nome, descricao, preco) VALUES (nextval('hibernate_sequence'), 'Smartphone', 'iPhone 13 Pro 256GB', 6700.0);
+INSERT INTO Produto(id, nome, descricao, preco) VALUES (nextval('hibernate_sequence'), 'Tablet', 'iPad Air 4 64GB', 4200.0);
