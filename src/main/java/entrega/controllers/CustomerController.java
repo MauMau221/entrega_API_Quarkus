9fa40ade9e22352d2a59a4ba90851488aa1ba605
MauMau221 <mauriciobgsp@gmail.com>
2025-09-22 16:38:07 -0300

    update controller

--- src/main/java/entrega/controllers/CustomerController.java
+++ src/main/java/entrega/controllers/CustomerController.java
@@ -1,4 +1,31 @@
 package entrega.controllers;
 
+import entrega.models.Customer;
+import entrega.repositories.CustomerRepository;
+import entrega.services.CustomerService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/customers")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CustomerController {
+
+    @Inject
+    CustomerRepository customerRepository;
+    @Inject
+    CustomerService customerService;
+
+    @GET
+    public List<Customer> list() {
+        return customerService.listAll();
+    }
+
+    @POST
+    public Customer create(Customer customer) {
+        return customerService.create(customer);
+    }
 }
